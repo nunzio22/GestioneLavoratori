0fbf054ccd4a58ab746b53ec7efea22885040384
nunzio22 <nunzio22598@gmail.com>
2019-10-07 17:59:26 +0200

    fine 1 giornata

--- GestioneLavoratori/Inserimento.cs
+++ GestioneLavoratori/Inserimento.cs
@@ -8,5 +8,11 @@ namespace GestioneLavoratori
 {
     class Inserimento
     {
+        public static void insert()
+        {
+                Console.WriteLine("Che tipo di Lavoratore si vuole inserire ?" + Environment.NewLine
+                    + "1 Lavoratore dipendente " + Environment.NewLine
+                    + "2 Lavoratore autonomo");
+        }
     }
 }
